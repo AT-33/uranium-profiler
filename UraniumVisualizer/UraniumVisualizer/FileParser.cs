44bea5441d066b9c0ed4b00a71f243021d67555c
Dmitry Ishoev <Dra90D1m@gmail.com>
2021-05-04 01:46:04 +0500

    Edited FileParser and created a folder for tests

--- UraniumVisualizer/UraniumVisualizer/FileParser.cs
+++ UraniumVisualizer/UraniumVisualizer/FileParser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace UraniumVisualizer
 {
@@ -8,9 +10,20 @@ namespace UraniumVisualizer
     /// </summary>
     public class FileParser
     {
+        private string FileName { get; }
+
         public FileParser(string fileName)
         {
-            throw new NotImplementedException();
+            FileName = fileName;
+        }
+
+        private static EventType GetEventType(char c)
+        {
+            if (c == 'B')
+                return EventType.Start;
+            if (c == 'E')
+                return EventType.End;
+            throw new SystemException("Could not determine the type of event");
         }
 
         /// <summary>
@@ -19,7 +32,49 @@ namespace UraniumVisualizer
         /// <returns></returns>
         public IEnumerable<FunctionRecord> Parse()
         {
-            throw new NotImplementedException();
+            var stack = new Stack<ProfilerEvent>();
+
+            using (var f = new StreamReader(FileName))
+            {
+                var functionPosition = 0;
+
+                while (!f.EndOfStream)
+                {
+                    var str = f.ReadLine();
+                    //PE, pe - ProfilerEvent
+                    var timeStampPE = int.Parse(Regex.Match(str, @"\d+").Value);
+                    var typePE = GetEventType(str[0]);
+                    var pe = new ProfilerEvent(timeStampPE, typePE);
+
+                    if (stack.Count == 0 || pe.Type == EventType.Start)
+                    {
+                        stack.Push(pe);
+                        var functionName = Regex.Match(str.Substring(1), @"\D+").Value;
+                        yield return new FunctionRecord(functionName, functionPosition, pe.TimeStamp);
+                        functionPosition++;
+                    }
+                    else
+                    {
+                        stack.Pop();
+                        functionPosition--;
+                    }
+                }
+            }
+        }
+
+        public void Test()
+        {
+            using (var fstream = new FileStream(@"H:\GitH\uranium-profiler\UraniumVisualizer\UraniumVisualizer\ParserTests\testReturn.txt",
+                                                FileMode.OpenOrCreate))
+            {
+                foreach (var e in Parse())
+                {
+                    var arr = System.Text
+                        .Encoding.Default
+                        .GetBytes($"Name: {e.Name}\n    PositionX: {e.PositionX}, PositionY: {e.PositionY}\n\n");
+                    fstream.Write(arr, 0, arr.Length);
+                }
+            }
         }
     }
 }
