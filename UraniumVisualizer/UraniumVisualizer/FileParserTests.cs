d9d7753750b1134b16ff188cfb5a5513fd2d2cf6
Dmitry Ishoev <26188825+NetVoobrazhenia@users.noreply.github.com>
2021-05-04 23:43:12 +0500

    Исправил недочеты

--- UraniumVisualizer/UraniumVisualizer/FileParserTests.cs
+++ UraniumVisualizer/UraniumVisualizer/FileParserTests.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace UraniumVisualizer
@@ -12,8 +7,6 @@ namespace UraniumVisualizer
     [TestFixture]
     class FileParserTests
     {
-        const string Folder = @"H:\GitH\uranium-profiler\UraniumVisualizer\UraniumVisualizer\ParserTests\";
-
         [TestCase(@"H:\GitH\uranium-profiler\UraniumVisualizer\UraniumVisualizer\ParserTests\test1.ups",
                   @"H:\GitH\uranium-profiler\UraniumVisualizer\UraniumVisualizer\ParserTests\test1Solve.txt")]
         [TestCase(@"H:\GitH\uranium-profiler\UraniumVisualizer\UraniumVisualizer\ParserTests\test2.ups",
@@ -22,35 +15,14 @@ namespace UraniumVisualizer
         public void TestCases(string testPath, string testSolvePath)
         {
             var p = new FileParser(testPath);
-            var testName = Regex.Match(testPath, @"\\test\d+").Value;
-            var testReturnPath = $@"{Folder}{testName}Return.txt";
-            CreatTestRerurnInFolder(p, testReturnPath);
-            Assert.AreEqual(ReadFile(testReturnPath), ReadFile(testSolvePath));
-        }
-
-        private static void CreatTestRerurnInFolder(FileParser p, string testReturnPath)
-        {
-            using (var fstream = new FileStream(testReturnPath, FileMode.OpenOrCreate))
+            var testReturn = new StringBuilder();
+            foreach (var e in p.Parse())
             {
-                foreach (var e in p.Parse())
-                {
-                    var t = new string('\t', e.PositionY);
-                    var arr = Encoding.Default
-                        .GetBytes(t + $"Name: {e.Name}, PositionX: {e.PositionX}, " +
-                                  $"PositionY: {e.PositionY}, Duration: {e.Duration}\r\n");
-                    fstream.Write(arr, 0, arr.Length);
-                }
+                var t = new string('\t', e.PositionY);
+                testReturn.Append(t + $"Name: {e.Name}, PositionX: {e.PositionX}, " +
+                          $"PositionY: {e.PositionY}, Duration: {e.Duration}\r\n");
             }
-        }
-
-        private static string ReadFile(string path)
-        {
-            using (FileStream fstream = File.OpenRead(path))
-            {
-                byte[] array = new byte[fstream.Length];
-                fstream.Read(array, 0, array.Length);
-                return Encoding.Default.GetString(array);
-            }
+            Assert.AreEqual(testReturn.ToString(), File.ReadAllText(testSolvePath));
         }
     }
 }
